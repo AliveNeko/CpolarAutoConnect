71e7eaafcac92bbe7c5b447c791ca89ef0051ff0
LiXin-Link <1191792984@qq.com>
2022-10-11 17:43:42 +0800

    暂存

--- CpolarAutoConnect.Core/Main.cs
+++ CpolarAutoConnect.Core/Main.cs
@@ -4,3 +4,17 @@ using CpolarAutoConnect.Core.Util;
 var coreSetting = SettingUtil.GetSetting<CoreSetting>();
 
 Console.WriteLine(coreSetting);
+
+HttpClient httpClient = new HttpClient();
+
+try
+{
+    HttpResponseMessage httpResponseMessage = await httpClient.GetAsync("https://dashboard.cpolar.com/status");
+    Console.WriteLine(httpResponseMessage);
+    
+}
+catch (HttpRequestException e)
+{
+    Console.WriteLine("\nException Caught!");
+    Console.WriteLine("Message :{0} ", e.Message);
+}
